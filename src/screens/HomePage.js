810bec2a0f2e6aed762a5a4c1c1a8d50b9c84537
ESD <ESD@DESKTOP-SNRCVRJ>
2020-10-01 16:17:20 +0600

    HomePage design complete

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,16 +1,46 @@
 import React from "react";
-import { Text, View, Button, TouchableOpacity, StyleSheet, Image } from "react-native";
+import {
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
 
 const HomePage = () => {
   return (
-    <View
-    style={style.viewStyle}
-    >
-        <Image
-        source = {require("./../../assets/IUT.png")}
+    <View style={style.viewStyle}>
+      <Image
+        source={require("./../../assets/IUT.png")}
         style={style.Image}
-        ></Image>
-      <Text>HomePage</Text>
+      ></Image>
+      <Text style={style.textStyle}>Department Of CSE</Text>
+      <Text style={style.textStyle2}>Program: SWE</Text>
+      <Text></Text>
+      <TouchableOpacity
+        onPress={function () {
+          console.log("go to profile page");
+        }}
+      >
+        <Text style={style.touchableText}>My Profile</Text>
+      </TouchableOpacity>
+      <Text></Text>
+      <Button
+        title="Semesters"
+        onPress={function () {
+          console.log("go to semester list");
+        }}
+      ></Button>
+      <Text></Text>
+      <Button
+        title="Faculty members"
+        onPress={function () {
+          console.log("Go to faculty list");
+        }}
+      ></Button>
+      <Text></Text>
+      <Text></Text>
     </View>
   );
 };
@@ -18,17 +48,26 @@ const HomePage = () => {
 const style = StyleSheet.create({
   textStyle: {
     fontSize: 30,
-    color: "black",
+  },
+
+  textStyle2: {
+    fontSize: 25,
+  },
+
+  touchableText: {
+    fontWeight: "800",
+    fontSize: 30,
+    borderColor: "aquamarine",
+    borderWidth: 2,
+    borderRadius: 5,
+    backgroundColor: "aquamarine",
   },
 
   viewStyle: {
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: "lightcyan",
   },
-
-  imageStyle: {
-
-  }
 });
 
 export default HomePage;
